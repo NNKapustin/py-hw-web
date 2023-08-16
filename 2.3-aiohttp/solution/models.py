c0f438529f25880037cac7ac639b920e350c9f21
nikita <nikita.n.kapustin@gmail.com>
2023-08-16 14:05:23 +0300

    Solve docker problem, Task 2 completed

--- 2.3-aiohttp/solution/models.py
+++ 2.3-aiohttp/solution/models.py
@@ -11,7 +11,7 @@ PG_USER = os.getenv("PG_USER", "user")
 PG_PASSWORD = os.getenv("PG_PASSWORD", "1234")
 PG_DB = os.getenv("PG_DB", "netology")
 PG_HOST = os.getenv("PG_HOST", "127.0.0.1")
-PG_PORT = os.getenv("PG_PORT", "5431")
+PG_PORT = os.getenv("PG_PORT", "5432")
 
 PG_DSN = f"postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:{PG_PORT}/{PG_DB}"
 
